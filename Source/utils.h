cb67ca6ae63e5f9af2a045bdde8f1040d30ce900
tranvinhhung99 <tranvinhhung99@gmail.com>
2020-05-07 13:01:31 +0700

    update header

--- Source/utils.h
+++ Source/utils.h
@@ -27,6 +27,62 @@ namespace utils{
       cv::Point anchor=cv::Point(-1, -1)
 
   );
+
+  /* Create 3x3 Sobel Filter from parameters.
+   *
+   * @param kernel: Output kernel
+   * @param angle: Can only be 0, 45, 90, 135
+   * @param depth: Currently only support CV_8S
+   *
+   */ 
+  void createSobelFilter(cv::OutputArray kernel, unsigned char angle, int depth=CV_8S);
+
+  /* Create 3x3 Prewitt Filter from parameters.
+   *
+   * @param kernel: Output kernel
+   * @param angle: Can only be 0, 45, 90, 135
+   * @param depth: Currently only support CV_8S
+   *
+   */ 
+  void createPrewittFilter(cv::OutputArray kernel, unsigned char angle, int depth=CV_8S);
+
+
+  /* Apply Gaussian Filter from parameters.
+   *
+   * @param src: Input image. Must be CV_32F depth
+   * @param dst: Output image
+   * @param kernel_size: Kernel size of Gaussian Filter. Must be odd number
+   *
+   */ 
+  void applyGaussianFilter(cv::InputArray src,
+      cv::OutputArray dst,
+      unsigned char kernel_size
+  );
+
+
+  /* Detect edge by Sobel algorithm
+   *
+   * @param src: Input image, must be in CV_8U
+   * @param dst: Output Image, will output in CV_16S
+   *
+   */
+  void detectBySobel(cv::InputArray src, cv::OutputArray dst);
+
+  /* Detect edge by Prewitt algorithm
+   *
+   * @param src: Input image, must be in CV_8U
+   * @param dst: Output Image, will output in CV_16S
+   *
+   */
+  void detectByPrewitt(cv::InputArray src, cv::OutputArray dst);
+
+  /* Detect edge by Prewitt algorithm
+   *
+   * @param src: Input image, must be in CV_8U
+   * @param dst: Output Image, will output in CV_16S
+   *
+   */
+  void detectByPrewitt(cv::InputArray src, cv::OutputArray dst);
 };
 
 #endif //UTILS_H_
